04513305cd103ac915160dae4191e9275b9f18a3
root <root@vps-20627086.vps.ovh.ca>
2025-10-20 05:33:41 +0000

    Fully working batch pay pre-check db for weeks

--- BK-18-10-444AM/resources/views/layouts/components/footer.blade.php
+++
@@ -1,9 +0,0 @@
-<footer class="footer mt-auto py-3 text-center">
-    <div class="container">
-        <a href="javascript:void(0);" class="text-dark fw-medium">Intelboard</a>
-        <span class="text-muted"> | Copyright © 2024 - <span id="year"></span> #
-            All
-            rights reserved
-        </span>
-    </div>
-</footer>
